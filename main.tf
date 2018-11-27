f7365c0da49c795f13ad65baa13ef39d4f56595c
SoaadMahmoud <37635397+SoaadMahmoud@users.noreply.github.com>
2018-11-27 15:12:57 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,24 +3,37 @@ variable "prefix" {
 }
 
 resource "azurerm_resource_group" "rgone" {
-  name     = "rgone"
+  name     = "CIF-CONV-COFFRE"
   location = "West Europe"
 }
 resource "azurerm_resource_group" "rgtwo" {
-  name     = "rgtwo"
+  name     = "CIF-CONV-TU"
   location = "West Europe"
 }
-
 resource "azurerm_virtual_network" "rgone" {
   name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["172.16.0.0/16"]
   location            = "${azurerm_resource_group.rgone.location}"
   resource_group_name = "${azurerm_resource_group.rgone.name}"
 }
-
 resource "azurerm_subnet" "mgmt" {
   name                 = "internal"
   resource_group_name  = "${azurerm_resource_group.rgone.name}"
   virtual_network_name = "${azurerm_virtual_network.rgone.name}"
-  address_prefix       = "10.0.2.0/24"
+  address_prefix       = "172.16.10.0/24"
 }
+
+resource "azurerm_virtual_network" "rgtwo" {
+  name                = "${var.prefix}-network"
+  address_space       = ["172.16.0.0/16"]
+  location            = "${azurerm_resource_group.rgtwo.location}"
+  resource_group_name = "${azurerm_resource_group.rgtwo.name}"
+  
+}
+resource "azurerm_subnet" "mgmt" {
+  name                 = "internal"
+  resource_group_name  = "${azurerm_resource_group.rgtwo.name}"
+  virtual_network_name = "${azurerm_virtual_network.rgtwo.name}"
+  address_prefix       = "172.16.20.0/24"
+}
+
